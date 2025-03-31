f2dc48a962b0b07822be82fc1c058a6de542dd75
K-IMjihun <kjh950901@gmail.com>
2025-03-31 13:57:43 +0900

    feat/네비게이션 추가 및 수정정

--- login/login.js
+++ login/login.js
@@ -1,26 +1,18 @@
-/**
- * Variables
- */
 const signupButton = document.getElementById('signup-button'),
     loginButton = document.getElementById('login-button'),
     userForms = document.getElementById('user_options-forms')
 
-/**
- * Add event listener to the "Sign Up" button
- */
 signupButton.addEventListener('click', () => {
   userForms.classList.remove('bounceRight')
   userForms.classList.add('bounceLeft')
 }, false)
 
-/**
- * Add event listener to the "Login" button
- */
 loginButton.addEventListener('click', () => {
   userForms.classList.remove('bounceLeft')
   userForms.classList.add('bounceRight')
 }, false)
 
+// 네비게이션
 fetch('../navi/navi.html')
 .then(response => response.text())
 .then(data => {
