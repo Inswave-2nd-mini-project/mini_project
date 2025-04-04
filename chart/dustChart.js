a633fdc33cba5907aaca08a32a677d9b307a9e74
K-IMjihun <kjh950901@gmail.com>
2025-04-04 17:44:51 +0900

    feat : 미세먼지 차트 구현중

--- chart/dustChart.js
+++ chart/dustChart.js
@@ -6,8 +6,50 @@ fetch('../navi/navi.html')
 })
 .catch(error => console.error('Error loading navi:', error));
 
-let currentWeekData = [];
-let lastWeekData = [];
+// 차트 생성
+var options = {
+    chart: {
+      height: 280,
+      type: "area"
+    },
+    dataLabels: {
+      enabled: false
+    },
+    series: [
+      {
+        name: "Series 1",
+        data: [45, 52, 38, 45, 19, 23, 2]
+      }
+    ],
+    fill: {
+      type: "gradient",
+      gradient: {
+        shadeIntensity: 1,
+        opacityFrom: 0.7,
+        opacityTo: 0.9,
+        stops: [0, 90, 100]
+      }
+    },
+    xaxis: {
+      categories: [
+        "01 Jan",
+        "02 Jan",
+        "03 Jan",
+        "04 Jan",
+        "05 Jan",
+        "06 Jan",
+        "07 Jan"
+      ]
+    }
+  };
+  
+  var chart = new ApexCharts(document.querySelector("#chart"), options);
+  
+  chart.render();
+  
+
+// let currentWeekData = [];
+// let lastWeekData = [];
 
 async function getServiceKey() {
 	const res = await fetch('../util/config.json');
@@ -58,4 +100,3 @@ async function fetchData() {
 }
 
 fetchData().catch(console.error);
-
