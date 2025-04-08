00f5c4abbfc5e39d2a60a8df0ff150af6670d188
K-IMjihun <kjh950901@gmail.com>
2025-04-08 13:56:18 +0900

    refactor : 배경색 통일 및 주석수정

--- main/Main_Page.js
+++ main/Main_Page.js
@@ -15,21 +15,20 @@
     let active = 4;
 	function loadShow(){
 		let stt = 0;
-		// Reset all items
 		for (let i = 0; i < items.length; i++) {
+			// 모든 슬라이드 초기화
 			items[i].style.transform = '';
 			items[i].style.zIndex = '';
 			items[i].style.filter = '';
 			items[i].style.opacity = '';
 		}
 	
-		// Active item (center)
+		// 중앙에 위치할 이미지
 		items[active].style.transform = `none`;
 		items[active].style.zIndex = 1;
 		items[active].style.filter = 'none';
 		items[active].style.opacity = 1;
-	
-		// Right side items
+
 		stt = 0;
 		for (let i = 1; i <= 2; i++) {
 			stt++;
@@ -37,10 +36,10 @@
 			items[index].style.transform = `translateX(${120 * stt}px) scale(${1 - 0.2 * stt}) perspective(16px) rotateY(-1deg)`;
 			items[index].style.zIndex = -stt;
 			items[index].style.filter = 'blur(5px)';
-			items[index].style.opacity = stt > 2 ? 0 : 0.6;
+			items[index].style.opacity = 0.6;
+			// items[index].style.opacity = stt == 2 ? 0 : 0.6;
 		}
 	
-		// Left side items
 		stt = 0;
 		for (let i = 1; i <= 2; i++) {
 			stt++;
@@ -48,7 +47,8 @@
 			items[index].style.transform = `translateX(${-120 * stt}px) scale(${1 - 0.2 * stt}) perspective(16px) rotateY(1deg)`;
 			items[index].style.zIndex = -stt;
 			items[index].style.filter = 'blur(5px)';
-			items[index].style.opacity = stt > 2 ? 0 : 0.6;
+			items[index].style.opacity = 0.6;
+			// items[index].style.opacity = stt == 2 ? 0 : 0.6;
 		}
 	}
     loadShow();
@@ -63,7 +63,7 @@ prev.onclick = function(){
     loadShow();
 }
 
-// 	//  이동할 html 링크
+//  이동할 html 링크
 	const links = [
 		"../BSH1/index.html", 					//칸반	
 		"../board/board.html",					//보드
@@ -72,7 +72,7 @@ prev.onclick = function(){
 		"../login/login.html"					//조 소개(?)
 	];
   
-	// 이미지 클릭 시 해당 html로 이동
+// 이미지 클릭 시 해당 html로 이동
 	items.forEach((item, index) => {
 		item.onclick = function() {
 			if (index === active) {
